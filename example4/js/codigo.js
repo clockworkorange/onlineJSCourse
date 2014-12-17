63513aa1e3d8bfe1d73cfee2e1b3661aee1ccd94
Julio Ripoll <julio.ripoll@ososo.es>
2014-12-17 15:49:44 +0100

    Added fourth exercise

---
+++ example4/js/codigo.js
@@ -0,0 +1,9 @@
+var valores = [true, 5, false, "hola", "adios", 2];
+
+alert(valores[3] > valores[4] ? valores[3] : valores[4]);
+alert(valores[0] || valores[2]);
+alert(valores[1] + valores[5]);
+alert(valores[1] - valores[5]);
+alert(valores[1] * valores[5]);
+alert(valores[1] / valores[5]);
+alert(valores[1] % valores[5]);
