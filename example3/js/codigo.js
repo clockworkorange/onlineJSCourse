a3b74e500cda721c1eb041d68c4c3f369d0b38fa
Julio Ripoll <julio.ripoll@ososo.es>
2014-12-17 15:37:21 +0100

    Added third execise

---
+++ example3/js/codigo.js
@@ -0,0 +1,5 @@
+var meses = ['enero', 'febrero', 'marzo', 'abril', 'mayo', 'junio', 'julio', 'agosto', 'septiembre', 'octubre', 'noviembre', 'diciembre'];
+
+for(i in meses) {
+	alert(meses[i]);	
+}
